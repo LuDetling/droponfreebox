5022b64e2f65c9283a36951ac61ddf6a363a0749
Lucas Detling <lucas.detling@gmail.com>
2023-07-11 17:40:21 +0200

    try to login

--- background.js
+++ background.js
@@ -1,58 +1,107 @@
-const host = 'http://mafreebox.freebox.fr'
+import { createHmac } from "crypto";
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        console.log(
-            `Storage key "${key}" in namespace "${namespace}" changed.`,
-            `Old value was "${JSON.stringify(oldValue)}", new value is "${JSON.stringify(newValue)}".`
-        );
 
-        if (!newValue.checked) return
+var hmac, result;
+hmac = createHmac("sha1", "i'm a secret!");
 
-        console.log(newValue);
+hmac.update("hello world!");
+hmac.update("hello nodejs!");
+result = hmac.digest("hex");
+console.log(result); //9e7b9239f03d2e03cb041a8518977ac84ab4c9b9
+// const host = 'http://mafreebox.freebox.fr'
 
-    }
-});
+// chrome.storage.onChanged.addListener((changes, namespace) => {
+//     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+//         console.log(
+//             `Storage key "${key}" in namespace "${namespace}" changed.`,
+//             `Old value was "${JSON.stringify(oldValue)}", new value is "${JSON.stringify(newValue)}".`
+//         );
 
-//créer une session token et pas une app token
+//         if (!newValue.checked) return
 
-const authorizeFreebox = async () => {
-    const dataToFetch = {
-        app_id: "fr.freebox.FreeboxMediaUploader",
-        app_name: "Freebox Media Uploader",
-        app_version: "0.0.1",
-        device_name: "Pc de Lucas"
-    }
+//         console.log(newValue);
 
-    try {
-        await fetch(host + "/api/v8/login/authorize/", {
-            headers: {
-                "Accept": 'application/json',
-                'Content-Type': 'application/json',
-            },
-            method: "POST",
-            body: JSON.stringify(dataToFetch)
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                authorizationProgress(data.result.track_id)
-            })
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     }
+// });
 
-const authorizationProgress = async (trackId) => {
-    try {
-        await fetch(host + "/api/v8/login/authorize/" + trackId)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-            })
-    } catch (error) {
-        console.log(error);
-    }
-}
 
-authorizeFreebox()
+// // // créer une session token et pas une app token
+// // const challengeValue = async () => {
+// //     try {
+// //         await fetch(host + "/api/v4/login/")
+// //             .then((res) => res.json())
+// //             .then((data) => {
+// //                 console.log("challenge value : " + JSON.stringify(data));
+// //             })
+// //     } catch (error) {
+// //         console.log(error);
+// //     }
+// // }
+
+// // challengeValue()
+
+// // const authorizeFreebox = async () => {
+// //     const dataToFetch = {
+// //         app_id: "fr.freebox.FreeboxMediaUploader",
+// //         app_name: "Freebox Media Uploader",
+// //         app_version: "0.0.1",
+// //         device_name: "Pc de Lucas"
+// //     }
+
+// //     try {
+// //         await fetch(host + "/api/v8/login/authorize/", {
+// //             headers: {
+// //                 "Accept": 'application/json',
+// //                 'Content-Type': 'application/json',
+// //             },
+// //             method: "POST",
+// //             body: JSON.stringify(dataToFetch)
+// //         })
+// //             .then((res) => res.json())
+// //             .then((data) => {
+// //                 console.log(data);
+// //                 authorizationProgress(data.result.track_id)
+// //             })
+// //     } catch (error) {
+// //         console.log(error);
+// //     }
+// // }
+
+// // const authorizationProgress = async (trackId) => {
+// //     try {
+// //         await fetch(host + "/api/v8/login/authorize/" + trackId)
+// //             .then(res => res.json())
+// //             .then(data => {
+// //                 console.log(data);
+// //             })
+// //     } catch (error) {
+// //         console.log(error);
+// //     }
+// // }
+
+// // authorizeFreebox()
+
+// const openSession = async () => {
+//     const dataToFetch = {
+//         app_id: "fr.freebox.FreeboxMediaUploader",
+//         password: "h/SAkbsCt3NFUS550tyPb9ZjYYoTAlBk"
+//     }
+//     try {
+//         await fetch(host + "/api/v8/login/session/", {
+//             method: "POST",
+//             headers: {
+//                 "Accept": 'application/json',
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(dataToFetch)
+//         })
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data)
+//             })
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// openSession()
